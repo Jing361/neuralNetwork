139ffc4321d112aa944bb90ec42f3c933efef740
Chris Samuelson <chris.sam55@gmail.com>
2016-06-09 15:29:55 -0400

    Added glm

--- header/network.hh
+++ header/network.hh
@@ -1,25 +1,19 @@
 #ifndef __NETWORK_HH__
 #define __NETWORK_HH__
 
-#include"layer.hh"
+#include<vector>
 
 class network{
 private:
-  layer mInput;
-  layer mOutput;
+	std::vector<unsigned int> mSizes;
+	std::vector<unsigned int> mBiases;
+	std::vector<unsigned int> mWeights;
 
 public:
-  network(unsigned int inSize, unsigned int outSize);
-
-  template<class iterator>
-  void train(iterator begin, iterator end){
-    while(*begin != *end){
-      train(begin->first, begin->second);
-      ++begin;
-    }
-  }
-  void train(double input, double output);
-  double ask(double val) const;
+	template<class iterator>
+  network(iterator begin, iterator end):
+		mSizes(begin, end){
+	}
 };
 
 #endif
